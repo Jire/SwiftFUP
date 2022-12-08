38d3580a8eea228e9e4651e798fcf45bb7be83ff
Jire <thomasgnappo@gmail.com>
2022-12-07 21:43:31 -0500

    Version `1.6.0`: removed idle and fulfilled checks

--- server/src/main/kotlin/org/jire/swiftfup/server/net/FileServerChannelInitializer.kt
+++ server/src/main/kotlin/org/jire/swiftfup/server/net/FileServerChannelInitializer.kt
@@ -2,10 +2,8 @@ package org.jire.swiftfup.server.net
 
 import io.netty.channel.ChannelInitializer
 import io.netty.channel.socket.SocketChannel
-import io.netty.handler.timeout.IdleStateHandler
 import org.jire.swiftfup.server.net.codec.FileServerRequestDecoder
 import org.jire.swiftfup.server.net.codec.FileServerRequestHandler
-import java.util.concurrent.TimeUnit
 
 /**
  * @author Jire
@@ -16,13 +14,8 @@ class FileServerChannelInitializer(
 
     override fun initChannel(ch: SocketChannel) {
         ch.pipeline()
-            .addLast(IdleStateHandler(true, 0, 0, IDLE_TIMEOUT_SECONDS, TimeUnit.SECONDS))
             .addLast(FileServerRequestDecoder())
             .addLast(FileServerRequestHandler(fileRequestResponses))
-    }
-
-    private companion object {
-        private const val IDLE_TIMEOUT_SECONDS = 120L
     }
 
 }
--- server/src/main/kotlin/org/jire/swiftfup/server/net/codec/FileServerRequestHandler.kt
+++ server/src/main/kotlin/org/jire/swiftfup/server/net/codec/FileServerRequestHandler.kt
@@ -2,9 +2,6 @@ package org.jire.swiftfup.server.net.codec
 
 import io.netty.channel.ChannelHandlerContext
 import io.netty.channel.SimpleChannelInboundHandler
-import io.netty.handler.timeout.IdleStateEvent
-import it.unimi.dsi.fastutil.ints.Int2BooleanMap
-import it.unimi.dsi.fastutil.ints.Int2BooleanOpenHashMap
 import org.jire.swiftfup.server.FilePair
 import org.jire.swiftfup.server.net.FileRequestResponses
 import java.io.IOException
@@ -16,55 +13,14 @@ class FileServerRequestHandler(
     private val responses: FileRequestResponses
 ) : SimpleChannelInboundHandler<FilePair>() {
 
-    private var fulfilledChecksums = false
-    private val fulfilledPairs: Int2BooleanMap = Int2BooleanOpenHashMap()
-
     override fun channelRead0(ctx: ChannelHandlerContext, msg: FilePair) {
         if (msg.bitpack == FilePair.checksumsFilePair.bitpack) {
-            if (fulfilledChecksums) {
-                throw IllegalStateException("Cannot request checksums multiple times!")
-            }
-            fulfilledChecksums = true
-
             ctx.write(responses.checksumsResponse.retainedDuplicate(), ctx.voidPromise())
         } else {
-            if (fulfilledPairs.get(msg.bitpack)) {
-                throw IllegalStateException("File pair was already fulfilled: $msg")
-            }
-            fulfilledPairs[msg.bitpack] = true
-
             val response = responses[msg]
                 ?: throw IllegalStateException("Request was null for $msg")
 
             ctx.write(response.retainedDuplicate(), ctx.voidPromise())
-        }
-    }
-
-    override fun channelReadComplete(ctx: ChannelHandlerContext) {
-        ctx.flush()
-
-        val channel = ctx.channel()
-        if (!channel.isWritable) {
-            channel.config().isAutoRead = false
-        }
-    }
-
-    override fun channelWritabilityChanged(ctx: ChannelHandlerContext) {
-        val channel = ctx.channel()
-        if (channel.isWritable) {
-            channel.config().isAutoRead = true
-            ctx.read()
-        } else {
-            ctx.flush()
-            if (!channel.isWritable) {
-                channel.config().isAutoRead = false
-            }
-        }
-    }
-
-    override fun userEventTriggered(ctx: ChannelHandlerContext, evt: Any) {
-        if (evt is IdleStateEvent) {
-            ctx.close()
         }
     }
 
